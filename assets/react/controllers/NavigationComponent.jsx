a1a427e8cf13afb87a8156ae241a623f4cc4db53
AlonahAmelie <amelie.abdallah@hotmail.fr>
2023-02-19 21:20:05 +0100

    make a footer and update (a little) navbar

--- assets/react/controllers/NavigationComponent.jsx
+++ assets/react/controllers/NavigationComponent.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import classNames from 'classnames';
 import { CaretDownIcon } from '@radix-ui/react-icons';
-import './style.css';
-import LogoAmeliedev from '../../../../images/logo-ameliedev.svg';
+import './NavigationComponentStyle.css';
+import LogoAmeliedev from '../../images/logo-ameliedev.svg';
 
 const NavigationMenuDemo = () => {
     return (
@@ -69,10 +69,33 @@ const NavigationMenuDemo = () => {
                 </NavigationMenu.Item>
 
                 <NavigationMenu.Item>
-                    <NavigationMenu.Link className="NavigationMenuLink" target={"blank"} href="https://www.linkedin.com/in/alonahamelie/">
-                        LinkedIn
+                    <NavigationMenu.Trigger className="NavigationMenuTrigger">
+                        Réseaux
                         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 1C1.44772 1 1 1.44772 1 2V13C1 13.5523 1.44772 14 2 14H13C13.5523 14 14 13.5523 14 13V2C14 1.44772 13.5523 1 13 1H2ZM3.05 6H4.95V12H3.05V6ZM5.075 4.005C5.075 4.59871 4.59371 5.08 4 5.08C3.4063 5.08 2.925 4.59871 2.925 4.005C2.925 3.41129 3.4063 2.93 4 2.93C4.59371 2.93 5.075 3.41129 5.075 4.005ZM12 8.35713C12 6.55208 10.8334 5.85033 9.67449 5.85033C9.29502 5.83163 8.91721 5.91119 8.57874 6.08107C8.32172 6.21007 8.05265 6.50523 7.84516 7.01853H7.79179V6.00044H6V12.0047H7.90616V8.8112C7.8786 8.48413 7.98327 8.06142 8.19741 7.80987C8.41156 7.55832 8.71789 7.49825 8.95015 7.46774H9.02258C9.62874 7.46774 10.0786 7.84301 10.0786 8.78868V12.0047H11.9847L12 8.35713Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                    </NavigationMenu.Link>
+                        <CaretDownIcon className="CaretDown" aria-hidden />
+                    </NavigationMenu.Trigger>
+                    <NavigationMenu.Content className="NavigationMenuContent">
+                        <ul className="List two">
+                            <ListItem title="LinkedIn"  target={"blank"} href="https://www.linkedin.com/in/alonahamelie/">
+                                LinkedIn
+                            </ListItem>
+                            <ListItem title="Getting started" href="/docs/primitives/overview/getting-started">
+                                A quick tutorial to get you up and running with Radix Primitives.
+                            </ListItem>
+                            <ListItem title="Styling" href="/docs/primitives/overview/styling">
+                                Unstyled and compatible with any styling solution.
+                            </ListItem>
+                            <ListItem title="Animation" href="/docs/primitives/overview/animation">
+                                Use CSS keyframes or any animation library of your choice.
+                            </ListItem>
+                            <ListItem title="Accessibility" href="/docs/primitives/overview/accessibility">
+                                Tested in a range of browsers and assistive technologies.
+                            </ListItem>
+                            <ListItem title="Releases" href="/docs/primitives/overview/releases">
+                                Radix Primitives releases and their changelogs.
+                            </ListItem>
+                        </ul>
+                    </NavigationMenu.Content>
                 </NavigationMenu.Item>
 
                 <NavigationMenu.Indicator className="NavigationMenuIndicator">
